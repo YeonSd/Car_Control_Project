6bd669fb4e0072e3db67b8ea1aa29738f177fd98
YeonsuJ <jysjyg34@gmail.com>
2025-05-14 18:02:54 +0900

    Add lcd_display test code

---
+++ M4/lcd_display/Core/Src/main.c
@@ -0,0 +1,174 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+#define I2C_ADDR 0x27 // I2C address of the PCF8574
+#define RS_BIT 0 // Register select bit
+#define EN_BIT 2 // Enable bit
+#define BL_BIT 3 // Backlight bit
+#define D4_BIT 4 // Data 4 bit
+#define D5_BIT 5 // Data 5 bit
+#define D6_BIT 6 // Data 6 bit
+#define D7_BIT 7 // Data 7 bit
+
+uint8_t backlight_state = 1;
+
+/* Private variables */
+I2C_HandleTypeDef hi2c1;
+
+/* Function prototypes */
+void SystemClock_Config(void);
+static void MX_I2C1_Init(void);
+
+void lcd_write_nibble(uint8_t nibble, uint8_t rs);
+void lcd_send_cmd(uint8_t cmd);
+void lcd_send_data(uint8_t data);
+void lcd_init();
+void lcd_write_string(char *str);
+void lcd_set_cursor(uint8_t row, uint8_t column);
+void lcd_clear(void);
+void lcd_backlight(uint8_t state);
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  MX_I2C1_Init();
+
+  lcd_init();
+  lcd_backlight(1);
+
+  lcd_clear();
+  lcd_set_cursor(0, 0);
+  lcd_write_string("lcd display");
+
+  while (1)
+  {
+    HAL_Delay(1000);
+  }
+}
+
+void lcd_write_nibble(uint8_t nibble, uint8_t rs) {
+  uint8_t data = nibble << D4_BIT;
+  data |= rs << RS_BIT;
+  data |= backlight_state << BL_BIT;
+  data |= 1 << EN_BIT;
+  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
+  HAL_Delay(1);
+  data &= ~(1 << EN_BIT);
+  HAL_I2C_Master_Transmit(&hi2c1, I2C_ADDR << 1, &data, 1, 100);
+}
+
+void lcd_send_cmd(uint8_t cmd) {
+  uint8_t upper_nibble = cmd >> 4;
+  uint8_t lower_nibble = cmd & 0x0F;
+  lcd_write_nibble(upper_nibble, 0);
+  lcd_write_nibble(lower_nibble, 0);
+  if (cmd == 0x01 || cmd == 0x02) {
+    HAL_Delay(2);
+  }
+}
+
+void lcd_send_data(uint8_t data) {
+  uint8_t upper_nibble = data >> 4;
+  uint8_t lower_nibble = data & 0x0F;
+  lcd_write_nibble(upper_nibble, 1);
+  lcd_write_nibble(lower_nibble, 1);
+}
+
+void lcd_init() {
+  HAL_Delay(50);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(5);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x03, 0);
+  HAL_Delay(1);
+  lcd_write_nibble(0x02, 0);
+  lcd_send_cmd(0x28);
+  lcd_send_cmd(0x0C);
+  lcd_send_cmd(0x06);
+  lcd_send_cmd(0x01);
+  HAL_Delay(2);
+}
+
+void lcd_write_string(char *str) {
+  while (*str) {
+    lcd_send_data(*str++);
+  }
+}
+
+void lcd_set_cursor(uint8_t row, uint8_t column) {
+  uint8_t address = (row == 0) ? 0x00 : 0x40;
+  address += column;
+  lcd_send_cmd(0x80 | address);
+}
+
+void lcd_clear(void) {
+  lcd_send_cmd(0x01);
+  HAL_Delay(2);
+}
+
+void lcd_backlight(uint8_t state) {
+  backlight_state = (state) ? 1 : 0;
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_I2C1_Init(void)
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
